57928adfcbf45cc7a913f751fbad8940dfb6c91e
samwance <odintsovvans@gmail.com>
2024-01-04 11:35:22 +0600

    added login, register, created subscription app

--- content/models.py
+++ content/models.py
@@ -10,25 +10,28 @@ class Album(models.Model):
     name = models.CharField()
     description = models.CharField()
     preview = models.ImageField(upload_to='content_previews/', **NULL)
-    is_free = models.BooleanField()
-    price = models.DecimalField(max_digits=10,decimal_places=2, **NULL)
+
+    def __str__(self):
+        return self.name
 
 
 class Content(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, **NULL)
     name = models.CharField()
     description = models.CharField()
     preview = models.ImageField(upload_to='content_previews/', **NULL)
-    video_file = models.FileField(upload_to='video/', **NULL)
-    audio_file = models.FileField(upload_to='audio/', **NULL)
+    post_time = models.DateTimeField(auto_now_add=True)
+    is_free = models.BooleanField()
 
     CATEGORY_CHOICES = [
         ('Art', 'Art'),
-        ('Music', 'Music'),
+        ('Poems', 'Poems'),
         ('Education', 'Education'),
         ('Entertainment', 'Entertainment'),
-        ('Literature', 'Literature'),
-        ('Not_selected', 'Not selected')
+        ('Other', 'Other')
     ]
-    category = models.CharField(choices=CATEGORY_CHOICES, default='Not_selected')
+    category = models.CharField(choices=CATEGORY_CHOICES, default='Other')
+
+    def __str__(self):
+        return self.name
